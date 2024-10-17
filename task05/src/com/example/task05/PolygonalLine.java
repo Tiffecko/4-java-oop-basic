3889774dedb58d1a919c2f87181c6c522e988cde
Gamer <zxz@>
2024-10-17 19:43:30 +0500

    fixed

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -4,6 +4,7 @@ package com.example.task05;
  * Ломаная линия
  */
 public class PolygonalLine {
+    private Point[] points = new Point[0];
 
     /**
      * Устанавливает точки ломаной линии
@@ -11,8 +12,11 @@ public class PolygonalLine {
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
-    }
+        Point[] newPoints = new Point[points.length];
+        for (int i = 0; i < points.length; i++) {
+            newPoints[i] = new Point(points[i].getX(), points[i].getY());
+        }
+        this.points = newPoints;    }
 
     /**
      * Добавляет точку к ломаной линии
@@ -20,7 +24,14 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        Point[] newPoints = new Point[this.points.length + 1];
+
+        for (int i = 0; i < points.length; i++) {
+            newPoints[i] = new Point(points[i].getX(), points[i].getY());
+        }
+
+        newPoints[newPoints.length - 1] = new Point(point.getX(), point.getY());
+        points = newPoints;
     }
 
     /**
@@ -30,8 +41,8 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
-    }
+        Point point = new Point(x,y);
+        addPoint(point);    }
 
     /**
      * Возвращает длину ломаной линии
@@ -39,8 +50,11 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double len = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            len += points[i].getLength(points[i + 1]);
+        }
+        return len;
     }
 
 }
